166efe76a8b38521327f389d3a269ffc65bf2652
TonyS-dev <santiagor.acarlos@gmail.com>
2026-01-20 22:28:06 -0500

    chore: initial project structure

---
+++ src/test/java/com/acnexus/projectmanagement/ProjectmanagementApplicationTests.java
@@ -0,0 +1,15 @@
+package com.acnexus.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ProjectmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
